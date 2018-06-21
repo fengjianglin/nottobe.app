bca36f65dde2e7c7bbb1f6c90966f96e97831156
fengjianglin <fengjianglin@manlanvideo.com>
2018-06-21 11:09:14 +0800

    port 7000

--- src/main/java/app/nottobe/web/Index.java
+++ src/main/java/app/nottobe/web/Index.java
@@ -3,6 +3,7 @@ package app.nottobe.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -11,5 +12,16 @@ public class Index {
 	@GetMapping("/")
 	public String home() {
 		return "index";
+	}
+
+	@GetMapping("/mp")
+	@ResponseBody
+	public String mp(String signature, String timestamp, String nonce, String echostr) {
+		return echostr;
+	}
+
+	@GetMapping("/s")
+	public String s() {
+		return "s";
 	}
 }
