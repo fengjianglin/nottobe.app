557acfb28a4c2952368be32b5ffc3696d5bada26
fengjianglin <fjl@lenovo>
2019-08-09 09:58:00 +0800

    去掉redis

--- src/main/java/app/nottobe/component/SessionService.java
+++ src/main/java/app/nottobe/component/SessionService.java
@@ -1,34 +1,31 @@
 package app.nottobe.component;
 
-import java.util.concurrent.TimeUnit;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import app.nottobe.bean.User;
 
-@SuppressWarnings({ "rawtypes", "unchecked" })
 @Service
 public class SessionService {
 
 	@Value("${session.expire.days}")
 	private Long sessionExpireDays;
 
-	@Autowired
-	private RedisTemplate redisTemplate;
+	private Map<String, User> redisTemplate = new HashMap<String, User>();
 
 	public void save(String sessionId, User user) {
 		if (StringUtils.hasText(sessionId)) {
-			redisTemplate.opsForValue().set(sessionId, user, sessionExpireDays, TimeUnit.DAYS);
+			redisTemplate.put(sessionId, user);
 		}
 	}
 
 	public User find(String sessionId) {
 		try {
-			return (User) redisTemplate.opsForValue().get(sessionId);
+			return (User) redisTemplate.get(sessionId);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
